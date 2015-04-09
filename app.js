1f19b288f2f0bb9f09eaa471a1fd21ddcb218ee5
afolachea <afolachea@gmail.com>
2015-04-08 21:30:20 -0700

    First Chat functionality

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var cool = require('cool-ascii-faces');
+
+server.listen(3000);
+
+app.get('/', function(req, res){
+	res.send(cool());
+});
+
+app.get('/chat/', function(req, res){
+	res.sendFile(__dirname + '/chat.html');
+});
+
+io.sockets.on('connection', function(socket){
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', data);
+	})
+})
